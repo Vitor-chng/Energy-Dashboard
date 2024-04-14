b2c59419821176fda5bb607d620e79e7107cc1b9
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-04-14 14:01:34 -0300

    test postgress

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ const app = express()
 const port = 3001
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello World!');
+  res.status(200).send('Automação S2');
 })
+
+const energy_model = require('./energyRepository')
+
+app.use(express.json())
+
+app.get('/consumo', (req, res) => {
+    energy_model.getConsumo()
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
