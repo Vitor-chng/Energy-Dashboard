eb2ae2aa9ace4af385afd7a86e8c39ea65d50b68
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-04-15 22:32:46 -0300

    test axios

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { AxiosProvider, Request, Get, Delete, Head, Post, Put, Patch, withAxios } from 'react-axios'
 import EnergyChart from './EnergyChart';
 
 function App() {
@@ -8,7 +9,21 @@ function App() {
       <header className="App-header">
         <h1>Dashboard de Energia</h1>
       </header>
-      <EnergyChart />
+      <Get url="http://localhost:3001/consumo" >
+        {(error, response, isLoading, makeRequest, axios) => {
+          if(error) {
+            return (<div>Something bad happened: {error.message} <button onClick={() => makeRequest({ params: { reload: true } })}>Retry</button></div>)
+          }
+          else if(isLoading) {
+            return (<div>Loading...</div>)
+          }
+          else if(response !== null) {
+            return (<div>{response.data.message} <button onClick={() => makeRequest({ params: { refresh: true } })}>Refresh</button></div>)
+          }
+          return (<div>Default message before request is made.</div>)
+        }}
+      </Get>
+      {/* <EnergyChart /> */}
     </div>
   );
 }
